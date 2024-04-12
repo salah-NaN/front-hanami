06f9442b147d2be6fd43b9929cb8371af0b54e83
luisy9 <luisdeharo4@gmail.com>
2024-04-12 19:02:15 +0200

    refactor cardHotTrends

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { SearchBar, Banner, CardHotTrendItem } from "../components";
+import CardBox from "../components/cardItem/CardBox";
 
 export const Inicio = () => {
   //Seteamos el valor por defecto que sea null de useRef
@@ -26,19 +27,8 @@ export const Inicio = () => {
       <div className="">
         <SearchBar moveToSearchBar={moveToSearchBar} />
       </div>
-      <div className="w-11/12 mx-auto">
-        <div className="grid grid-cols-2 sm:grid sm:grid-cols-2 md:grid md:grid-cols-6 lg:grid lg:grid-cols-6 xl:grid xl:grid-cols-6 max-auto gap-3">
-          {hotTrends?.slice(0, 2).map((hotTrend) => (
-            <div className="border-none w-full col-span-2 sm:col-span-2 md:col-span-3 lg:col-span-2 h-64">
-              <CardHotTrendItem hotTrend={hotTrend} />
-            </div>
-          ))}
-          {hotTrends?.slice(2, 6).map((hotTrend) => (
-            <div className="border-none w-full col-span-2 sm:col-span-2 md:col-span-3 lg:col-span-2 h-64">
-              <CardHotTrendItem hotTrend={hotTrend} />
-            </div>
-          ))}
-        </div>
+      <div className="">
+        <CardBox hotTrends={hotTrends} />
       </div>
     </>
   );
