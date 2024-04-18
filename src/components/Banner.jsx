691529270aa541b97f99b494766a982ca2b92f26
luisy9 <luisdeharo4@gmail.com>
2024-04-18 13:26:59 +0200

    first commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -10,7 +10,11 @@ import DescripcionBanner from "./description/DescripcionBanner";
 
 const COLORS = ["#008000", "#00FF00", "#87E196", "#98FB98"];
 
-export const Banner = ({ paginacionScrollHome }) => {
+export const Banner = ({
+  paginacionScrollHome,
+  mainControladorFlecha,
+  isVisible,
+}) => {
   const color = useMotionValue(COLORS[0]);
   const backgroundImage = useMotionTemplate`radial-gradient(123% 123% at 50% 0%, #FFFFFF 50%, ${color})`;
 
@@ -27,10 +31,10 @@ export const Banner = ({ paginacionScrollHome }) => {
     <motion.div
       style={{ backgroundImage }}
       className="relative grid min-h-screen place-items-center overflow-hidden
-       bg-gray-950 px-2 py-24 text-gray-200"
+       bg-gray-950 px-2 py-13 xm:py-24 text-gray-200"
     >
       <div className="z-10 flex flex-col items-center w-10/12 lg:w-11/12 mx-auto h-full relative">
-        <div className="flex xp:w-96 md:w-full ">
+        <div className="flex xp:w-96 md:w-full">
           <h1
             className="w-full text-center bg-gradient-to-br from-black to-slate-300
             bg-clip-text xs:text-[2.30rem] xs:tracking-wide xs:text-pretty
@@ -44,11 +48,16 @@ export const Banner = ({ paginacionScrollHome }) => {
             Toda la belleza de Cataluña en un solo clic
           </h1>
         </div>
-        <div className="pt-8">
+        <div className="pt-3 xm:pt-8">
           <DescripcionBanner />
         </div>
-        <div className="h-screen absolute bottom-20 lg:absolute lg:bottom-20 xl:absolute xl:bottom-29 2xl:absolute 2xl:bottom-80">
-          <Flecha paginacionScrollHome={paginacionScrollHome} />
+        <div className="w-full h-full">
+          <div className="fixed bottom-10 left-1/2 transform -translate-x-1/2 z-10">
+            <Flecha paginacionScrollHome={paginacionScrollHome} />
+          </div>
+          {/* <div className="flex w-full h-full justify-center items-end cursor-pointer"> */}
+          {/* <Flecha /> */}
+          {/* </div> */}
         </div>
       </div>
     </motion.div>
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -5,13 +5,20 @@ import { SearchBar, Banner, NavBar } from "../components";
 import CardBox from "../components/cardItem/CardBox";
 import SliderItems from "../components/SliderItems/SliderItems";
 import { CardHotTrendItem } from "../components";
+import { motion, useInView, useAnimation } from "framer-motion";
 
 export const Inicio = () => {
   let url = "http://localhost:3000/api/";
   //Seteamos el valor por defecto que sea null de useRef
   const moveToSearchBar = useRef(null);
+  const isSearchBarComponent = useRef(null);
   const [hotTrends, setHotTrends] = useState([]);
   const [actividadOrPuntoInteres, setActividadOrPuntoInteres] = useState();
+
+  const scrollBuscadorRef = useRef(false);
+  const isInView = useInView(scrollBuscadorRef);
+  const mainControladorFlecha = useAnimation();
+  const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
     // fetch(url + `puntos_interes/;/;/;`)
@@ -31,6 +38,21 @@ export const Inicio = () => {
       .catch((error) => console.log(error));
   }, []);
 
+  useEffect(() => {
+    console.log(isInView)
+    isInView === true
+      // ? mainControladorFlecha.start({
+      //     opacity: 0,
+      //     transitionDuration: 0,
+      //   })
+      // : mainControladorFlecha.start({
+      //     opacity: 1,
+      //     transitionDuration: 0,
+      //   });
+    ? setIsVisible(false)
+    : setIsVisible(true);
+  }, [isInView]);
+
   const paginacionScrollHome = () => {
     moveToSearchBar.current.scrollIntoView({ behavior: "smooth" });
   };
@@ -39,22 +61,27 @@ export const Inicio = () => {
     <div className="">
       <NavBar />
       <div className="w-full">
-        <Banner paginacionScrollHome={paginacionScrollHome} />
+        <div className="">
+          <Banner
+            paginacionScrollHome={paginacionScrollHome}
+            mainControladorFlecha={mainControladorFlecha}
+            isVisible={isVisible}
+          />
+        </div>
       </div>
-      <div className="w-10/12 mx-auto hidden md:block">
-        <SearchBar moveToSearchBar={moveToSearchBar} />
+      <div className="" ref={scrollBuscadorRef}>
+        <div
+          className="w-10/12 mx-auto hidden md:block"
+          ref={isSearchBarComponent}
+        >
+          <SearchBar moveToSearchBar={moveToSearchBar} />
+        </div>
+
+        <div className="w-10/12 mx-auto"></div>
+        <div className="pt-10 md:w-[89%] lg:w-[87%] xl:w-[87%] 2xl:w-[85%] w-10/12 mx-auto relative"></div>
+        <Mapa />
+        <Footer />
       </div>
-      <div className="w-10/12 mx-auto">
-        <CardBox hotTrends={hotTrends} />
-      </div>
-      <div className="pt-10 md:w-[89%] lg:w-[87%] xl:w-[87%] 2xl:w-[85%] w-10/12 mx-auto relative">
-        <SliderItems
-          url={url}
-          setActividadOrPuntoInteres={setActividadOrPuntoInteres}
-          actividadOrPuntoInteres={actividadOrPuntoInteres}
-        />
-      </div>
-      <Footer />
     </div>
   );
 };
