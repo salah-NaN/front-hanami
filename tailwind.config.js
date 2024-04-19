81c95a7a4dd58dc832c82a39dd430234de52c6fa
luisy9 <luisdeharo4@gmail.com>
2024-04-19 11:47:02 +0200

    button navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ export default {
       fontFamily: {
         dmSans: "DM Sans",
       },
+      boxShadow: {
+        button: '0px 8px 0px 0px white',
+        press: '0px 3px 0px 0px white'
+      },
+      bgBack: {
+        'redd': "#FFFFFF"
+      }
     },
     screens: {
       'xs': '320px',
@@ -14,22 +21,6 @@ export default {
       'xp': '425px',
       ...defaultTheme.screens,
     },
-    // screens: {
-    //   'xs': "320px",
-    //   // => @media (min-width: 320px) { ... }
-    //   'ml': "375px",
-    //   // => @media (min-width: 375px) { ... }
-    //   'mx': "425px",
-    //   // => @media (min-width: 425px) { ... }
-    //   'md': "640px",
-    //   // => @media (min-width: 640px) { ... }
-    //   'lg': "1024px",
-    //   // => @media (min-width: 1024px) { ... }
-    //   'xl': "1280px",
-    //   // => @media (min-width: 1280px) { ... }
-    //   '2xl': "1536px"
-    //   // => @media (min-width: 1536px) { ... }
-    // },
   },
   plugins: [],
 };
