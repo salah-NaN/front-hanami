ef82a94507a5375df6dd5062996fa8f087b33e29
Salah Eddin <salah.extras@gmail.com>
2024-04-20 14:40:08 +0200

    done

---
+++ src/components/botones/ButtonPrimary.jsx
@@ -0,0 +1,10 @@
+const ButtonPrimary = () => {
+    
+    return(
+        <button className=" px-8 py-2.5 rounded-lg bg-[#53cd68] border-[#53cd68] border-2 shadow-md transition-all duration-300 hover:bg-[#4bb75d] hover:border-[#4bb75d] hover:text-[#d9d9d9]  text-[#fafafa]   ">
+            Pulsa 
+        </button>
+    )
+}
+
+export default ButtonPrimary
---
+++ src/components/botones/ButtonSecondary.jsx
@@ -0,0 +1,10 @@
+const ButtonSecondary = () => {
+    
+    return(
+        <button className=" px-8 py-2.5 rounded-lg border-[#53cd68] border-2 shadow-md transition-all duration-300 hover:bg-[#d9d9d9] hover:text-[#4bb75d]  text-[#53cd68]   ">
+            Pulsa
+        </button>
+    )
+}
+
+export default ButtonSecondary
