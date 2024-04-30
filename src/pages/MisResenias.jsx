cbb235f3d448d74efbfc1fb70b4c45c7df41d8f2
Salah Eddin <salah.extras@gmail.com>
2024-04-30 10:44:27 +0200

    mquetacion miresenias

--- src/pages/MisResenias.jsx
+++ src/pages/MisResenias.jsx
@@ -12,7 +12,7 @@ export const MisResenias = () => {
     // fetch de todas las reseñas del cliente
     useEffect(() => {
 
-        fetch(URL + '/cliente/resenias/', {credentials: 'include'})
+        fetch(URL + '/cliente/resenias/', { credentials: 'include' })
             .then(res => res.json())
             .then(res => {
                 // console.log(res)
@@ -25,12 +25,21 @@ export const MisResenias = () => {
 
 
     return (
-        <div className="mt-20 flex flex-col gap-4" >
-            {
-                resenias.length > 0 
-                ? resenias.map((resenia, index) => <Resenia resenia={resenia} key={index} />)
-                : <p>No hay resenias</p>
-            }
+        <div className="mt-20">
+            <h2 className="text-3xl w-fit mx-auto mb-4
+            sm:text-4xl" >{resenias.length + ' evaluaciones'}</h2>
+            <div className=" flex flex-col gap-4
+            sm:w-10/12 sm:mx-auto
+            md:w-9/12
+            lg:w-8/12
+            xl:w-7/12">
+                {
+                    resenias.length > 0
+                        ? resenias.map((resenia, index) => <Resenia resenia={resenia} key={index} />)
+                        : <p>No hay resenias</p>
+                }
+
+            </div>
 
         </div>
     )
